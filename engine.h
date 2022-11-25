3bbbcef470dd46315eb4227e8465a87b3fa0767d
Wilton Nicolas <78822853+f3noxy@users.noreply.github.com>
2022-11-25 14:08:12 -0300

    Sistema de histórico, pontuação e final de jogo
    - Opção de adicionar o nome para o jogador;
    - Função para determinar o máximo de perguntas realizadas;
    - Função de parar as perguntas e levar uma pontuação reduzida;
    - Função para determinar a pontuação obtida;
    - Função para salvar o histórico de jogadores, com seu nome e pontuação;
    - Função para salvar os vencedores do jogo no pódio.

--- engine.h
+++ engine.h
@@ -4,6 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdbool.h> 
+#include <string.h>
 
 int gerarNumeroAleatorio(int maxRange){
 	
@@ -70,6 +74,160 @@ char obterAlternativaCorreta(int numeroResposta){
 	fclose(alternativas);
 	
 	return alternativa[0];
+	
+}
+
+int checarPremioParar(int perguntaAtual){
+	switch(perguntaAtual){
+		case 0:
+			return 0;
+			break;
+		case 1:
+			return 1000;
+			break;
+		case 2:
+			return 2000;
+			break;
+		case 3:
+			return 3000;
+			break;
+		case 4:
+			return 4000;
+			break;
+		case 5:
+			return 5000;
+			break;
+		case 6:
+			return 10000;
+			break;
+		case 7:
+			return 20000;
+			break;
+		case 8:
+			return 30000;
+			break;
+		case 9:
+			return 40000;
+			break;
+		case 10:
+			return 50000;
+			break;
+		case 11:
+			return 100000;
+			break;
+		case 12:
+			return 200000;
+			break;
+		case 13:
+			return 300000;
+			break;
+		case 14:
+			return 400000;
+			break;
+		case 15:
+			return 500000;
+			break;
+	}
+}
+
+int checarPremioPerder(int perguntaAtual){
+	switch(perguntaAtual){
+		case 0:
+			return 0;
+			break;
+		case 1:
+			return 500;
+			break;
+		case 2:
+			return 1000;
+			break;
+		case 3:
+			return 1500;
+			break;
+		case 4:
+			return 2000;
+			break;
+		case 5:
+			return 2500;
+			break;
+		case 6:
+			return 5000;
+			break;
+		case 7:
+			return 10000;
+			break;
+		case 8:
+			return 15000;
+			break;
+		case 9:
+			return 20000;
+			break;
+		case 10:
+			return 25000;
+			break;
+		case 11:
+			return 50000;
+			break;
+		case 12:
+			return 100000;
+			break;
+		case 13:
+			return 150000;
+			break;
+		case 14:
+			return 200000;
+			break;
+		case 15:
+			return 0;
+			break;
+	}
+}
+
+bool file_exists (char *filename) {
+  struct stat   buffer;   
+  return (stat (filename, &buffer) == 0);
+}
+
+void salvarVencedores(char nome[99]){
+	
+	FILE *vencedores;
+	
+	if(file_exists("./data/vencedores.txt")){
+		vencedores = fopen("./data/vencedores.txt", "a");
+	}
+	else{
+		vencedores = fopen("./data/vencedores.txt", "w");
+		fputs("--- Vencedores do Jogo ---\n\n", vencedores);
+	}
+	
+	fputs(nome, vencedores);
+	
+	fclose(vencedores);
+	
+}
+
+void salvarHistorico(char nome[99], int premio){
+	
+	FILE *historico;
+	char premioString[7];
+	
+	if(file_exists("./data/historico.txt")){
+		historico = fopen("./data/historico.txt", "a");
+	}
+	else{
+		historico = fopen("./data/historico.txt", "w");
+		fputs("--- Historico ---\n\n", historico);
+	}
+	
+	nome[strlen(nome) - 1] = ' ';
+	snprintf(premioString, 7, "%d", premio);
+	
+	fputs(nome, historico);
+	fputs("- R$", historico);
+	fputs(premioString, historico);
+	fputs("\n", historico);
+	
+	fclose(historico);
 	
 }
 
--- game.c
+++ game.c
@@ -19,37 +19,72 @@ int main(){
 	while(1){
 		if(menuEscolha[0] == '1'){
 			
+			char jogador[99];
+			int i;
+			
+			printf("Digite o seu nome: ");
+			fgets(jogador, 98, stdin);
 			system("cls");
 			
-			int numeroAleatorio = gerarNumeroAleatorio(300), perguntaIndice = 1, respostaIndice = 2, alternativaCorretaIndice = 1801, caracterInvalido;
-			char respostaUsuario[99], alternativaCorreta = '2';
+			for(i = 0; i <= 15; i++){
 			
-			if(numeroAleatorio > 1){
-				perguntaIndice = (numeroAleatorio*6)-5;
-				respostaIndice = perguntaIndice+1;
-				alternativaCorretaIndice = 1800 + numeroAleatorio;
-				alternativaCorreta = obterAlternativaCorreta(alternativaCorretaIndice);
-			}
-			
-			exibirPergunta(perguntaIndice);
-			exibirRespostas(respostaIndice);
-			printf("\nDigite o número correspodente a alternativa correta: ");
-			do{
+				system("cls");
 				
-				caracterInvalido = 0;
-				fgets(respostaUsuario, sizeof respostaUsuario, stdin);
-				if(respostaUsuario[0] != '1' && respostaUsuario[0] != '2' && respostaUsuario[0] != '3' && respostaUsuario[0] != '4'){
-					caracterInvalido++;
-					printf("Por favor digite uma resposta válida.\n");
+				int numeroAleatorio = gerarNumeroAleatorio(300), perguntaIndice = 1, respostaIndice = 2, alternativaCorretaIndice = 1801, caracterInvalido, premioAtual, premioDerrota;
+				char respostaUsuario[99], alternativaCorreta = '2';
+				
+				if(numeroAleatorio > 1){
+					perguntaIndice = (numeroAleatorio*6)-5;
+					respostaIndice = perguntaIndice+1;
+					alternativaCorretaIndice = 1800 + numeroAleatorio;
+					alternativaCorreta = obterAlternativaCorreta(alternativaCorretaIndice);
 				}
 				
-			}while(caracterInvalido > 0);
+				exibirPergunta(perguntaIndice);
+				exibirRespostas(respostaIndice);
+				premioAtual = checarPremioParar(i);
+				premioDerrota = checarPremioPerder(i);
+				printf("\nDigite 5 caso queira parar ganhando R$%i\n", premioAtual);
+				printf("\nDigite o número correspodente a alternativa correta: ");
+				do{
+					
+					caracterInvalido = 0;
+					fgets(respostaUsuario, sizeof respostaUsuario, stdin);
+					if(respostaUsuario[0] != '1' && respostaUsuario[0] != '2' && respostaUsuario[0] != '3' && respostaUsuario[0] != '4' && respostaUsuario[0] != '5'){
+						caracterInvalido++;
+						printf("Por favor digite uma resposta válida.\n");
+					}
+					
+				}while(caracterInvalido > 0);
 			
-			if(respostaUsuario[0] == alternativaCorreta){
-				printf("\nParabéns você acertou.\n\n");
-			}
-			else{
-				printf("\nQue pena, você errou.\n\n");
+				
+				if(respostaUsuario[0] == alternativaCorreta){
+					if(i < 15){
+						printf("\nParabéns você acertou.\n\n");
+						system("pause");
+					}
+					else{
+						printf("\nParabéns você acertou todas as perguntas necessárias para o 1 milhão de reais!\n\n");
+						system("pause");
+						system("cls");
+						salvarHistorico(jogador, 1000000);
+						salvarVencedores(jogador);
+					}
+				}
+				else if(respostaUsuario[0] == '5'){
+					printf("\nVocê escolheu parar, e ganhou R$%i.\n\n", premioAtual);
+					system("pause");
+					system("cls");
+					salvarHistorico(jogador, premioAtual);
+					break;
+				}
+				else{
+					printf("\nQue pena, você errou. Mas ganhou R$%i.\n\n", premioDerrota);
+					system("pause");
+					system("cls");
+					salvarHistorico(jogador, premioDerrota);
+					break;
+				}
 			}
 			
 			printf("Escolha uma das opções a seguir digitando o numero correspodente:\n");
